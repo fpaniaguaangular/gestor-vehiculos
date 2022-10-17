e8d550c2555e6f252ae557699c0ddce716edb5b0
fpaniaguaangular <102974105+fpaniaguaangular@users.noreply.github.com>
2022-10-17 21:29:15 +0200

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MenuPrincipalComponent } from './components/menu-principal/menu-principal.component';
+import { ListadoVehiculosComponent } from './components/listado-vehiculos/listado-vehiculos.component';
+import { CreadorVehiculosComponent } from './components/creador-vehiculos/creador-vehiculos.component';
+
+//ANGULAR MATERIAL
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuPrincipalComponent,
+    ListadoVehiculosComponent,
+    CreadorVehiculosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/creador-vehiculos/creador-vehiculos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-creador-vehiculos',
+  templateUrl: './creador-vehiculos.component.html',
+  styleUrls: ['./creador-vehiculos.component.css']
+})
+export class CreadorVehiculosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
